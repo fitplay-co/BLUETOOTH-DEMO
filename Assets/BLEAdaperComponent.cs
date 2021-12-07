dba9442f9fa6fb7b0237ec577b8c4cb007ce90c5
needle1989 <46319708+needle1989@users.noreply.github.com>
2021-12-07 15:45:25 +0800

    Initial commit

---
+++ Assets/BLEAdaperComponent.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+public class BLEAdaperComponent
+{
+    public enum BKConnectionStatus
+    {
+        None = 0,
+        Initialized,
+        Scanning,
+        DeviceDiscovered,
+        DeviceConnected,
+        ServiceScanning,
+        ServiceConnected,
+    }
+
+    //mac可以改成你们自己骑行设备的mac 不用扫码 连接起来更方便
+    public const string MacAddrSZ = "BC:97:40:40:87:46";
+    public const string MacAddrCD = "BC:97:40:40:87:CB";
+
+    public ReactiveProperty<bool> Connected { get; private set; } = new ReactiveProperty<bool>(false);
+    public ReactiveProperty<bool> Scanning { get; private set; } = new ReactiveProperty<bool>(false);
+
+    public ReactiveProperty<BKConnectionStatus> BKStatus { get; private set; } =
+        new ReactiveProperty<BKConnectionStatus>(BKConnectionStatus.None);
+
+    //这三个不用动 设备里固定的
+    private string ServiceUuid = "69400001-b5a3-f393-e0a9-e50e24dcca99";
+    private string DownCharacteristicUuid = "69400003-b5a3-f393-e0a9-e50e24dcca99";
+    private string UpCharacteristicUuid = "69400002-b5a3-f393-e0a9-e50e24dcca99";
+    private string MacAddress;
+    private List<Action<byte[]>> onDataActions = new List<Action<byte[]>>();
+
+    /// <summary>
+    /// 扫描蓝牙并连接bk设备
+    /// </summary>
+    /// <param name="macAddress">连接对象的mac地址</param>
+    /// <returns></returns>
+    public async UniTask<Subject<int>> ScanToConnectBK(string macAddress)
+    {
+        var s = new Subject<int>();
+        if (this.Scanning.Value || this.Connected.Value)
+        {
+            s.OnError(new Exception("连接中..."));
+        }
+
+        if (this.BKStatus.Value < BKConnectionStatus.Initialized)
+        {
+            var tsk = new UniTaskCompletionSource();
+            BluetoothLEHardwareInterface.Initialize(true, false, () => { tsk.TrySetResult(); }, (e) =>
+            {
+                var exception = new Exception(e);
+                s.OnError(exception);
+                tsk.TrySetException(exception);
+            });
+            try
+            {
+                await tsk.Task;
+                this.BKStatus.Value = BKConnectionStatus.Initialized;
+            }
+            catch (Exception e)
+            {
+                return s;
+            }
+        }
+
+        this.Scanning.Value = false;
+#if IOS
+        BluetoothLEHardwareInterface.ScanForPeripheralsWithServices(null,
+            (s1, s2) => { },
+            (addr, name, len, data) =>
+            {
+                if (data is { Length: 6 })
+                {
+                    if (BitConverter.ToString(data).Replace("-", ":") == macAddress)
+                    {
+                        this.BKStatus.Value = BKConnectionStatus.DeviceDiscovered;
+                        BluetoothLEHardwareInterface.ConnectToPeripheral(addr, (theAddr) =>
+                            {
+                                this.Connected.Value = true;
+                                this.MacAddress = theAddr;
+                                this.BKStatus.Value = BKConnectionStatus.DeviceConnected;
+                                this.BKStatus.Value = BKConnectionStatus.ServiceScanning;
+                                Debug.Log($"设备连接:{theAddr}");
+                            },
+                            null,
+                            (theAddr, theService, cUuid) =>
+                            {
+                                Debug.Log($"发现服务:{theService},发现特征:{cUuid}");
+                                if (theService.ToLower().Equals(this.ServiceUuid) && cUuid.ToLower().Equals(this.DownCharacteristicUuid))
+                                {
+                                    this.BKStatus.Value = BKConnectionStatus.ServiceConnected;
+                                    BluetoothLEHardwareInterface.SubscribeCharacteristicWithDeviceAddress(addr, theService, cUuid,
+                                        null,
+                                        (deviceAddress, deviceCharacteristic, rawData) => { OnRawData(rawData); });
+                                    BluetoothLEHardwareInterface.StopScan();
+                                }
+                            });
+                    }
+                }
+            });
+#else
+        BluetoothLEHardwareInterface.ScanForPeripheralsWithServices(null, (addr, deviceName) =>
+        {
+            this.Scanning.Value = true;
+            this.BKStatus.Value = BKConnectionStatus.Scanning;
+            if (addr.Equals(macAddress))
+            {
+                this.BKStatus.Value = BKConnectionStatus.DeviceDiscovered;
+                BluetoothLEHardwareInterface.ConnectToPeripheral(addr, (theAddr) =>
+                    {
+                        this.Connected.Value = true;
+                        this.MacAddress = theAddr;
+                        this.BKStatus.Value = BKConnectionStatus.DeviceConnected;
+                        this.BKStatus.Value = BKConnectionStatus.ServiceScanning;
+                        Debug.Log($"设备连接:{theAddr}");
+                    },
+                    null,
+                    (theAddr, theService, cUuid) =>
+                    {
+                        Debug.Log($"发现服务:{theService},发现特征:{cUuid}");
+                        if (theService.Equals(this.ServiceUuid) && cUuid.Equals(this.DownCharacteristicUuid))
+                        {
+                            this.BKStatus.Value = BKConnectionStatus.ServiceConnected;
+                            BluetoothLEHardwareInterface.SubscribeCharacteristicWithDeviceAddress(addr, theService, cUuid,
+                                null,
+                                (deviceAddress, deviceCharacteristic, rawData) => { OnRawData(rawData); });
+                            BluetoothLEHardwareInterface.StopScan();
+                        }
+                    });
+            }
+        });
+#endif
+        return s;
+    }
+
+    /// <summary>
+    /// 向bk发送数据
+    /// </summary>
+    /// <param name="rawData"></param>
+    public void SendToBk(sbyte[] rawData)
+    {
+        if (this.BKStatus.Value < BKConnectionStatus.ServiceConnected)
+        {
+            Debug.Log("无连接");
+            return;
+        }
+
+        // ReSharper disable once SuspiciousTypeConversion.Global
+        var sbytes = rawData.ToList();
+        sbyte b = 0;
+        foreach (sbyte sb in sbytes)
+        {
+            b += sb;
+        }
+
+        sbytes.Add(b);
+
+        //Debug.Log("开始发送: " + BitConverter.ToString((byte[]) (Array) sbytes.ToArray()));
+        BluetoothLEHardwareInterface.WriteCharacteristic(this.MacAddress, this.ServiceUuid, this.UpCharacteristicUuid,
+            sbytes.ToArray(), sbytes.Count, true, (s) =>
+            {
+                /*Debug.Log("发送成功");*/
+            });
+    }
+
+    public void DisconnectBK()
+    {
+        BluetoothLEHardwareInterface.DisconnectPeripheral(this.MacAddress, mac => { this.BKStatus.Value = BKConnectionStatus.Initialized; });
+    }
+
+    /// <summary>
+    /// 注册接收到蓝牙数据的回调
+    /// </summary>
+    /// <param name="handler"></param>
+    public void AddOnDataHandler(Action<byte[]> handler)
+    {
+        this.onDataActions.Add(handler);
+    }
+
+    public void RemoveOnDataHandler(Action<byte[]> handler)
+    {
+        this.onDataActions.Remove(handler);
+    }
+
+    public void RemoveAllOnDataHandler()
+    {
+        this.onDataActions = new List<Action<byte[]>>();
+    }
+
+    private void OnRawData(byte[] rawData)
+    {
+        foreach (var act in this.onDataActions)
+        {
+            act(rawData);
+        }
+    }
+}
---
+++ Assets/BLEProtocals.cs
@@ -0,0 +1,142 @@
+﻿/// <summary>
+/// ble发给单车的数据包
+/// </summary>
+public struct Ble2GameMessage
+{
+    /// <summary>
+    /// 0xAA
+    /// </summary>
+    public byte head;
+
+    /// <summary>
+    /// 0x02
+    /// </summary>
+    public byte cmd;
+
+    /// <summary>
+    /// 0x12
+    /// </summary>
+    public byte len;
+
+    /// <summary>
+    /// 转向角
+    /// </summary>
+    public float steer;
+
+    /// <summary>
+    /// 功率值，单位：瓦
+    /// </summary>
+    public uint power;
+
+    /// <summary>
+    /// 前刹车
+    /// </summary>
+    public float fBrake;
+
+    /// <summary>
+    /// 后刹车，浮点线性输入，0~1放大100倍
+    /// </summary>
+    public float rBrake;
+
+    /// <summary>
+    /// 重置 0或1， 按下／弹开
+    /// </summary>
+    public bool reset;
+
+    /// <summary>
+    /// 踏频 整数，次/分钟
+    /// </summary>
+    public uint cadence;
+
+    /// <summary>
+    /// 整数，公里/小时
+    /// </summary>
+    public uint speed;
+
+    /// <summary>
+    /// 档位1，正表示加档，负数表示减档
+    /// </summary>
+    public int gear1;
+
+    /// <summary>
+    /// 档位2，正表示加档，负数表示减档
+    /// </summary>
+    public int gear2;
+
+    /// <summary>
+    /// 循环检校
+    /// </summary>
+    public int check;
+}
+
+public struct Game2BleMessage
+{
+    /// <summary>
+    /// 0xAA
+    /// </summary>
+    public byte head;
+
+    /// <summary>
+    /// 0x02
+    /// </summary>
+    public byte cmd;
+
+    /// <summary>
+    /// 0x12
+    /// </summary>
+    public byte len;
+
+    /// <summary>
+    /// 档位1，正表示加档，负数表示减档
+    /// </summary>
+    public sbyte gear1;
+
+    /// <summary>
+    /// 档位2，正表示加档，负数表示减档
+    /// </summary>
+    public sbyte gear2;
+
+    /// <summary>
+    /// 外部阻力，单位：牛
+    /// </summary>
+    public short resistance;
+}
+
+public static class BLEProtocalsHelper
+{
+    public static sbyte[] CreateGame2BleMessage(int gear1, int gear2, int resistance, uint vibsel)
+    {
+        return new sbyte[]
+        {
+            unchecked ((sbyte) 0xAA), 
+            0x03, 
+            0x09, 
+            (sbyte) gear1, 
+            (sbyte) gear2, 
+            (sbyte) ((resistance & 0xFF00) >> 8),
+            (sbyte) (resistance), 
+            (sbyte) vibsel
+        };
+    }
+
+    public static Ble2GameMessage? ParseBleMessage(byte[] bytes)
+    {
+        if (bytes.Length < 15)
+        {
+            return null;
+        }
+
+        return new Ble2GameMessage()
+        {
+            steer = (((int) ((uint) bytes[3] << 8) + bytes[4]) - 500) / 500f,
+            power = (((uint) bytes[5] << 8) + bytes[6]),
+            fBrake = bytes[7] / 100f,
+            rBrake = bytes[8] / 100f,
+            reset = (uint) bytes[9] > 0,
+            cadence = (((uint) bytes[10] << 8) + bytes[11]),
+            speed = (((uint) bytes[12] << 8) + bytes[13]),
+            gear1 = (sbyte) bytes[14],
+            gear2 = (sbyte) bytes[15]
+        };
+    }
+}
